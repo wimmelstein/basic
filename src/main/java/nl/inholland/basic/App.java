5837bb8988a4310a71899af372a873dde5ce0b01
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2022-09-20 15:47:56 +0200

    Diamond problem interfaces

--- src/main/java/nl/inholland/basic/App.java
+++ src/main/java/nl/inholland/basic/App.java
@@ -1,7 +1,36 @@
 package nl.inholland.basic;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+interface A {
+    default void hello() {
+        System.out.println("Hello from A");
+    }
+
+    ;
+}
+
+interface B extends A {
+    @Override
+    default void hello() {
+        System.out.println("Hello from B");
     }
 }
+
+interface C extends A {
+    @Override
+    default void hello() {
+        System.out.println("Hello from C");
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        D d = new D();
+        d.hello();
+    }
+}
+
+class D implements B, C {
+
+}
+
+
